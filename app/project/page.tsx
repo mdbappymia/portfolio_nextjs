db6e2fd58fde6307ac4a7821f59af7cfe867969b
Md Bappy Mia <bmia249@radiantds.com>
2024-02-09 17:06:04 +0600

    nav active class added

---
+++ app/project/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectPage = () => {
+  return (
+    <div>
+      <h1>Project page</h1>
+    </div>
+  );
+};
+
+export default ProjectPage;
--- components/MainNavbar.tsx
+++ components/MainNavbar.tsx
@@ -1,28 +1,73 @@
 "use client";
 
-import { Avatar, DarkThemeToggle, Dropdown, Navbar } from "flowbite-react";
+import { Button, DarkThemeToggle, Navbar } from "flowbite-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MainNavbar() {
+  const pathname = usePathname();
   return (
-    <Navbar className="my-10 py-5" fluid rounded>
-      <Navbar.Brand href="/">
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+    <Navbar
+      className="my-10 py-6 !px-10 rounded-3xl border-gray-300 border shadow-sm"
+      fluid
+    >
+      <Navbar.Brand>
+        <Link
+          href="/"
+          className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white"
+        >
           Bappy
-        </span>
+        </Link>
       </Navbar.Brand>
       <div className="flex md:order-2">
+        <Button className="rounded-full bg-lime-500 dark:bg-lime-600 px-5">
+          Let&apos;s Talk
+        </Button>
         <DarkThemeToggle />
 
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link href="#" active>
+        <Link
+          href="/"
+          className={`${
+            pathname == "/" ? "font-bold text-lime-500" : ""
+          } text-lg hover:text-lime-500 transition-colors font-bold`}
+        >
           Home
-        </Navbar.Link>
-        <Navbar.Link href="#">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
+        </Link>
+        <Link
+          href="/about"
+          className={`${
+            pathname == "/about" ? "font-bold text-lime-500" : ""
+          } text-lg hover:text-lime-500 transition-colors font-bold`}
+        >
+          About
+        </Link>
+        <Link
+          href="/project"
+          className={`${
+            pathname == "/project" ? "font-bold text-lime-500" : ""
+          } text-lg hover:text-lime-500 transition-colors font-bold`}
+        >
+          Project
+        </Link>
+        <Link
+          href="/blog"
+          className={`${
+            pathname == "/blog" ? "font-bold text-lime-500" : ""
+          } text-lg hover:text-lime-500 transition-colors font-bold`}
+        >
+          Blog
+        </Link>
+        <Link
+          href="/contact"
+          className={`${
+            pathname == "/contact" ? "font-bold text-lime-500" : ""
+          } text-lg hover:text-lime-500 transition-colors font-bold`}
+        >
+          Contact
+        </Link>
       </Navbar.Collapse>
     </Navbar>
   );
